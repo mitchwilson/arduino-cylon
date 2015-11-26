3312ec99ff50747a1caaaab612b4cbedcb7b0542
Mitch Wilson <mitchwilson1969@gmail.com>
2015-11-25 23:32:45 -0600

    Add timeout to debounce push button

--- robot/5/push_button.js
+++ robot/5/push_button.js
@@ -1,13 +1,18 @@
 var arduino = require('../../arduino.json');
 var cylon = require('cylon');
+var pressed = false;
 cylon.robot({
     connections: arduino.connections,
     devices: arduino.five.devices,
     work: function(my) {
         my.button.on('push', function() {
-            setTimeout(function() {
-                my.led.toggle();
-            }, 10);
+            if(!pressed) {
+                pressed = true;
+                setTimeout(function() {
+                    my.led.toggle();
+                    pressed = false;
+                }, 5);
+            };
         });
     }
 }).start();
